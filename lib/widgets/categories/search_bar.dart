469d65302a01f7ff529c46f218db7a7b12e0541c
lilkr0k0dil <tgfff8057@gmail.com>
2024-09-25 23:56:24 +0300

    test

--- lib/widgets/categories/search_bar.dart
+++ lib/widgets/categories/search_bar.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_database/firebase_database.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class SearchInput extends StatefulWidget {
@@ -10,32 +10,66 @@ class SearchInput extends StatefulWidget {
 }
 
 class _SearchInputState extends State<SearchInput> {
+  final databaseReference = FirebaseDatabase.instance.reference();
+  final TextEditingController _searchController = TextEditingController();
+  List _searchResults = [];
+
+  void _searchDatabase() {
+    databaseReference.child('items').orderByChild('name').equalTo(_searchController.text).once().then((DataSnapshot snapshot) {
+      if (snapshot.value != null) {
+        setState(() {
+          _searchResults = snapshot.value.values.toList();
+        });
+      } else {
+        setState(() {
+          _searchResults = [];
+        });
+      }
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-        hintText: 'Поиск',
-        hintStyle:
-            GoogleFonts.roboto(color: Colors.grey), // Цвет текста подсказки
-        prefixIcon:
-            const Icon(Icons.search, color: Colors.grey), // Иконка поиска
-        filled: true, // Включение фона
-        fillColor: Colors.grey[300], // Цвет фона
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10.0), // Скругление углов
-          borderSide: BorderSide.none, // Убираем обводку
+    return Column(
+      children: [
+        TextField(
+          decoration: InputDecoration(
+          hintText: 'Поиск',
+          hintStyle:
+              GoogleFonts.roboto(color: Colors.grey), // Цвет текста подсказки
+          prefixIcon:
+              const Icon(Icons.search, color: Colors.grey), // Иконка поиска
+          filled: true, // Включение фона
+          fillColor: Colors.grey[300], // Цвет фона
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10.0), // Скругление углов
+            borderSide: BorderSide.none, // Убираем обводку
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10.0),
+            borderSide: BorderSide.none, // Убираем обводку в обычном состоянии
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10.0),
+            borderSide: BorderSide.none, // Убираем обводку при фокусе
+          ),
+          contentPadding: const EdgeInsets.symmetric(
+              vertical: 5.0, horizontal: 8.0), // Настройка отступов
         ),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10.0),
-          borderSide: BorderSide.none, // Убираем обводку в обычном состоянии
+        onChanged: (text) {
+          _searchDatabase();
+        },
         ),
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10.0),
-          borderSide: BorderSide.none, // Убираем обводку при фокусе
+        Expanded(
+          child: ListView.builder(
+            itemCount: _searchResults.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(_searchResults[index]['name']),
+              );
+            },
+          ),
         ),
-        contentPadding: const EdgeInsets.symmetric(
-            vertical: 5.0, horizontal: 8.0), // Настройка отступов
-      ),
+      ],
     );
   }
 }
